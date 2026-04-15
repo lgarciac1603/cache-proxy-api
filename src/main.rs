e123ac24d72acd0501bc2cc6ad0d9789b8b8b2f2
Fernando García <lfgc851@gmail.com>
2026-04-14 23:41:56 -0600

    Agregar dotenvy como dependencia y crear archivo .env.example

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use std::{ env };
 use axum::{ routing::get, Router };
 
 #[tokio::main]
 
 async fn main() {
-  let app = Router::new().route("/", get(|| async { "Hello world!" }));
+	let app = Router::new().route("/", get(|| async { "Hello world!" }));
+	let port = env::var("PORT").expect("PORT variable not found");
 
-	let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+	let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await.unwrap();
 	axum::serve(listener, app).await.unwrap();
 }
