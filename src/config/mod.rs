7766046c4419afe13f99a4f3fb0c0fc025b7454a
Fernando García <lfgc851@gmail.com>
2026-04-17 20:15:09 -0600

    Agregar configuración de aplicación y proxy: implementar carga de configuración desde variables de entorno y archivo JSON

---
+++ src/config/mod.rs
@@ -0,0 +1,3 @@
+pub mod config;
+
+pub use config::{ load_app_config, load_proxy_config };
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
+mod config;
 mod handlers;
 mod routes;
 
 use dotenvy::dotenv;
-use std::env;
 
 #[tokio::main]
 async fn main() {
 	dotenv().ok();
-	let app = routes::create_routes();
-	
-	let port = env::var("PORT").expect("PORT variable not found");
-	let app_url = format!("0.0.0.0:{port}");
 
+	let app_config = config::load_app_config()
+		.expect("Failed to load app config");
+	let proxy_config = config::load_proxy_config(&app_config.proxy_config_path)
+		.expect("Failed to load proxy config");
+
+	print!("App config: {:?}", app_config);
+	print!("Proxy config: {:?}", proxy_config);
+	
+	let app = routes::create_routes();
+	let app_url = format!("0.0.0.0:{}", app_config.port);
 	let listener = tokio::net::TcpListener::bind(&app_url).await.unwrap();
+
 	print!("Server running on: {}", app_url);
 	
 	axum::serve(listener, app).await.unwrap();
